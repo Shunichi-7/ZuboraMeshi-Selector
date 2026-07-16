700a407edaf14566b0f288a173ebed9850c9db08
Shunichi Fujita <tfjsn7@gamil.com>
2026-07-16 14:41:27 +0900

    ホーム画面とログイン機能を実装し、ログイン状態に応じた画面遷移とナビゲーション表示を実現するため

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.portfolio, name="portfolio"),
-]
+    path("home/", views.home, name="home"),
+] #ズボラ飯セレクターのホーム画面をブラウザに表示できるようにするため、「/home/」へアクセスしたときに、views.pyのhomeを呼び出す。
--- main/views.py
+++ main/views.py
@@ -6,5 +6,5 @@ def portfolio(request):
     return render(request, "main/portfolio.html")
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request, "main/home.html") #レシピ検索やレシピ投稿などの画面を表示できるようにするため、home.htmlを読み込み、ホーム画面を表示する。
 
