80b4a93ad2ae06d7c495ce8682f115ad2e992de5
Shunichi Fujita <tfjsn7@gamil.com>
2026-08-22 18:05:04 +0900

    ポートフォリオにPDF資料を追加

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2y4ef#9o9ew-*1hyw+ufc-0$-5g%7j67=g@@zi#516xi57yg4s'
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-local-development-only"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,6 +131,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "tfjsn7@gmail.com"
-EMAIL_HOST_PASSWORD = "zptv nwzl xhjl cjli"
+
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# パスワード再設定URLの有効期限：1時間
+PASSWORD_RESET_TIMEOUT = 60 * 60
--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from .models import Recipe, Favorite # レシピとお気に入り情報を使�
 # Create your views here.
 
 def portfolio(request):
-    return redirect("login")
+    return render(request, "main/portfolio.html")
 
 def home(request):
     return render(request, "main/home.html") #レシピ検索やレシピ投稿などの画面を表示できるようにするため、home.htmlを読み込み、ホーム画面を表示する。
